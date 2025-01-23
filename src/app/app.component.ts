81e78f8accce470ff303f1b4a2fbbf05e00dd59e
JermaStock <awakewii@yandex.ru>
2025-01-24 04:16:46 +0800

    tui-lib: tui-lib added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiRoot } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TuiRoot,
+  ],
   templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
   standalone: true,
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projection';
 }
